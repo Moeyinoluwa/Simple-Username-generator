abb73ea3da5063f23f649a9954d5cb3e4fe8b549
MoyinoluwaSobow <>
2021-10-19 01:15:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to Moe's username generator")
+
+city = input("What city did you grow up in? \n")
+
+pet = input("What is the name of your pet? \n")
+
+print("Your username could be " + city + "'s" + "_" + pet)
